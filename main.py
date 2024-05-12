32e9336aba0f58c29b6c1ef90358f38bae1dfcc3
AnshAnkushGarg <111407568+AnshAnkushGarg@users.noreply.github.com>
2024-05-12 10:41:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,12 @@ async def gameban(ctx, id: int):
     with open("banned.json", "r+") as file:
         data = json.load(file)
         data["banned_users"].append(id)
+        await ctx.channel.send(f"There are {len(data["banned_users"} banned users currently")
         file.seek(0)
         json.dump(data, file, indent=4)
-    print("banned")
+        
     await ctx.channel.send("Sucessfully banned")
+    
             
 
 

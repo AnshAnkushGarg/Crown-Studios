cfbcabd78ccc298a652f760cfef394058f21e1fa
AnshAnkushGarg <111407568+AnshAnkushGarg@users.noreply.github.com>
2024-05-12 10:00:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+import random
+import discord
+import os
+import requests
+from discord.ext import commands
+from discord.ext.commands import has_permissions
+import urllib
+from web_server import keep_alive
+
+keep_alive()
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+
+client = commands.Bot(command_prefix="!",intents=intents)
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+
+
+@client.command()
+async def gameban(ctx, id: int):
+    with open("banned.json", "r+") as file:
+        data = json.load(file)
+        data["banned_users"].append(id)
+        file.seek(0)
+        json.dump(data, file, indent=4)
+            
+
+
+
+client.run("MTIzODUzMTY5MDI4MDc3OTgxNg.GwF24M.0AjdD6RAa_e8559uzMqz1JqU6X7-IA3M3u1rfs")
+  

6aed954a6230eea9e517dc137908779ce0988d2b
AnshAnkushGarg <111407568+AnshAnkushGarg@users.noreply.github.com>
2024-05-12 21:12:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from web_server import keep_alive
 import random
 
 
+data_api = "https://ap-south-1.aws.data.mongodb-api.com/app/data-mahzb/endpoint/data/v1/action/updateOne"
+api_key = os.environ.get("MONGO_KEY")
+bans_id = "6640dd7a8e1d44b6e1654de4"
+header = header = {"Content-Type": "application/ejson", "Accept": "application/json", "apiKey": api_key}
 
 keep_alive()
 
@@ -26,17 +30,12 @@ async def on_ready():
 
 @client.command()
 async def gameban(ctx, id: int):
-    with open("banned.json", "r+") as file:
-        data = json.load(file)
-        data["banned_users"].append(id)
-        file.seek(0)
-        json.dump(data, file, indent=4)
+    data = {"dataSource": "Cluster0", "database": "CrownStudios", "collection": "BANS", "filter": {"_id": {"$oid": bans_id}}, "update": {"$push": {"bans": id}}}
+    r = requests.post(data_api, json=data, headers=header)
+    await ctx.channel.send(f"Sent Ban Request For {id}!")
+    
 
-@client.command()
-async def listbanned(ctx):
-    with open("banned.json", "r+") as file:
-        data = json.load(file)
-        await ctx.channel.send(data["banned_users"])
+
             
 
 
